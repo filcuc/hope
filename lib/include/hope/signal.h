081d227f08d9eccbdeec4225d764a2ba2d222c16
Filippo Cucchetto <filippocucchetto@gmail.com>
2018-04-06 23:09:10 +0200

    Added support for disconnecting and multiple handlers

--- lib/include/hope/signal.h
+++ lib/include/hope/signal.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <functional>
 #include <map>
 
@@ -15,6 +16,14 @@ public:
         return m_id;
     }
 
+    bool operator==(const Connection& other) const {
+        return m_id == other.m_id;
+    }
+
+    bool operator<(const Connection& other) const {
+        return m_id < other.m_id;
+    }
+
 private:
     const int64_t m_id;
 };
@@ -22,36 +31,54 @@ private:
 
 template<class ... Args>
 class Signal {
+    using Handler = std::function<void(Args...)>;
 public:
     void emit(Args&&... args) {
-        if (m_handler)
-            m_handler(std::forward<Args>(args)...);
+        for (const std::pair<Connection, Handler>& pair : m_handlers) {
+            assert(pair->second);
+            pair->second(std::forward<Args>(args)...);
+        }
     }
 
     template<typename Handler>
-    void connect(Handler handler) {
-        m_handler = std::move(handler);
+    Connection connect(Handler handler) {
+        auto it = m_handlers.emplace(m_next_connection_id++, handler);
+        return it.first->first;
+    }
+
+    void disconnect(Connection c) {
+        m_handlers.erase(c);
     }
 
 private:
-    std::function<void(Args...)> m_handler;
+    std::map<Connection, Handler> m_handlers;
+    int64_t m_next_connection_id = 0;
 };
 
 template <>
 class Signal<void> {
+    using Handler = std::function<void()>;
 public:
     void emit() {
-        if (m_handler)
-            m_handler();
+        for (const std::pair<Connection, Handler>& pair : m_handlers) {
+            assert(pair.second);
+            pair.second();
+        }
     }
 
     template<typename Handler>
-    void connect(Handler handler) {
-        m_handler = std::move(handler);
+    Connection connect(Handler handler) {
+        auto it = m_handlers.emplace(m_next_connection_id++, handler);
+        return it.first->first;
+    }
+
+    void disconnect(Connection c) {
+        m_handlers.erase(c);
     }
 
 private:
-    std::function<void()> m_handler;
+    std::map<Connection, Handler> m_handlers;
+    int64_t m_next_connection_id = 0;
 };
 
 }
