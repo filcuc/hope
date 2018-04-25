33d0d013ba73e65694764285e976c65f97c69942
Filippo Cucchetto <filippocucchetto@gmail.com>
2018-04-25 11:52:51 +0200

    Little improvements in the example

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,11 +9,15 @@ using namespace hope;
 class Consumer : public Object {
 public:
     void consume(int k, int j) {
-        std::cout << "Argh" << k << " " << j << std::endl;
+        std::cout << "Consuming with two args" << k << " " << j << std::endl;
     }
 
     void consume_2(std::unique_ptr<int> k) {
-        std::cout << "Argh" << *k << std::endl;
+        std::cout << "Consuming with movable arg" << *k << std::endl;
+    }
+
+    void consume_3() {
+        std::cout << "Consuming without any arg" << std::endl;
     }
 };
 
@@ -27,14 +31,20 @@ public:
         return m_signal_2;
     }
 
+    Signal<void>& produce_signal_3() {
+        return m_signal_3;
+    }
+
     void produce() {
         m_signal.emit(10, 40);
         m_signal_2.emit(std::unique_ptr<int>(new int(320)));
+        m_signal_3.emit();
     }
 
 private:
     Signal<int, int> m_signal;
     Signal<std::unique_ptr<int>> m_signal_2;
+    Signal<void> m_signal_3;
 };
 
 int main()
@@ -48,6 +58,7 @@ int main()
     Producer producer;
     producer.produce_signal().connect(&printer, &Consumer::consume);
     producer.produce_signal_2().connect(&printer, &Consumer::consume_2);
+    producer.produce_signal_3().connect(&printer, &Consumer::consume_3);
     producer.produce();
     e.exec();
     thread.wait();
