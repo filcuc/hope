dac782c0e27318f6860ed2376ccaeb3ae6cf385f
Filippo Cucchetto <filippocucchetto@gmail.com>
2018-04-28 22:53:40 +0200

    Added LGPLv3 header notice

--- lib/include/hope/private/indexsequence.h
+++ lib/include/hope/private/indexsequence.h
@@ -1,5 +1,23 @@
-#ifndef INDEXSEQUENCE_H
-#define INDEXSEQUENCE_H
+/*
+    Copyright (C) 2018 Filippo Cucchetto.
+    Contact: https://github.com/filcuc/hope
+
+    This file is part of the Hope library.
+
+    The Hope library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    The Hope library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Hope library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
 
 #if __cplusplus < 201402L
 
@@ -29,7 +47,5 @@ using make_index_sequence = typename index_sequence_builder<T>::result;
 #else
 
 #include <utility>
-
-#endif
 
 #endif
--- lib/src/thread.cpp
+++ lib/src/thread.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2018 Filippo Cucchetto.
+    Contact: https://github.com/filcuc/hope
+
+    This file is part of the Hope library.
+
+    The Hope library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    The Hope library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Hope library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "hope/thread.h"
 
 #include "hope/eventloop.h"
