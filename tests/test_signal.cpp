77b7c41eea85440e66968f33531ab424423de14d
Filippo Cucchetto <filippocucchetto@gmail.com>
2018-06-02 16:12:22 +0200

    Started testing signal

---
+++ tests/test_signal.cpp
@@ -0,0 +1,34 @@
+/*
+    Copyright (C) 2018 Filippo Cucchetto.
+    Contact: https://github.com/filcuc/hope
+
+    This file is part of the Hope library.
+
+    The Hope library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    The Hope library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Hope library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <hope/object.h>
+
+#include <gtest/gtest.h>
+
+using namespace hope;
+
+class SignalFixture : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+    }
+
+};
+
+TEST_F(SignalFixture, CreationTest) {
+}
